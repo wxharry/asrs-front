8d35ce661dec6acd13307e7914371322e99dd678
wxh <wxh170607@163.com>
2021-01-06 21:03:44 +0800

    update register

--- src/pages/reg/reg.vue
+++ src/pages/reg/reg.vue
@@ -1,51 +1,82 @@
 <template>
   <view class="content">
-    <view class="uni-list-cell uni-list-cell-pd">
-      <view class="ul">请申请加入交流群或使用好友邀请码</view>
+    <view class="input-group">
+      <view class="input-row border">
+        <text class="title">账号：</text>
+        <m-input
+          class="m-input"
+          type="text"
+          clearable
+          focus
+          v-model="username"
+          placeholder="请输入受邀人账号"
+        ></m-input>
+      </view>
+      <view class="input-row border">
+        <text class="title">密码：</text>
+        <m-input
+          type="password"
+          displayable
+          v-model="password"
+          placeholder="请输入受邀人密码"
+        ></m-input>
+      </view>
+      <view class="input-row border">
+        <text class="title">邮箱：</text>
+        <m-input
+          type="email"
+          v-model="email"
+          placeholder="请输入受邀人邮箱"
+        ></m-input>
+      </view>
     </view>
-    <!-- <view class="input-group">
-			<view class="input-row border">
-				<text class="title">账号：</text>
-				<m-input type="text" focus clearable v-model="username" placeholder="请输入账号"></m-input>
-			</view>
-			<view class="input-row border">
-				<text class="title">密码：</text>
-				<m-input type="password" displayable v-model="password" placeholder="请输入密码"></m-input>
-			</view>
-			<view class="input-row">
-				<text class="title">确认密码：</text>
-				<m-input type="password" displayable v-model="confirmPassword" placeholder="请确认密码"></m-input>
-			</view>
-		</view>
-		<view class="btn-row">
-			<button type="primary" class="primary" @tap="register">注册并登录</button>
-		</view> -->
+    <view class="btn-row">
+      <button
+        type="primary"
+        class="primary"
+        :loading="confirmBtnLoading"
+        @tap="open()"
+      >
+        点击测试
+      </button>
+    </view>
+    <uni-popup ref="popup" type="dialog">
+      <uni-popup-dialog
+        type="input"
+        title="确认"
+        content="是否确认邀请？"
+        :before-close="true"
+        @close="close"
+        @confirm="confirm"
+      ></uni-popup-dialog>
+    </uni-popup>
   </view>
 </template>
 
 <script>
-
+import mInput from "../../components/m-input.vue";
+import { uniPopupDialog } from "@dcloudio/uni-ui";
+import baseURL from "@/common/config.js";
 export default {
   components: {
-    // mInput,
+    mInput,
+    uniPopupDialog,
   },
   data() {
     return {
       username: "",
       password: "",
-      confirmPassword: "",
+      email: "",
+      confirmBtnLoading: false,
     };
   },
   methods: {
-    register() {
-      /**
-       * 客户端对账号信息进行一些必要的校验。
-       * 实际开发中，根据业务需要进行处理，这里仅做示例。
-       */
-      if (this.username.length < 3) {
+    checkIsValid() {
+      // TODO 检测学号格式
+      if (this.username.length != 8) {
         uni.showToast({
           icon: "none",
-          title: "账号最短为 3 个字符",
+          title: "账号格式有误",
         });
         return;
       }
@@ -56,51 +87,64 @@ export default {
         });
         return;
       }
-      if (this.password !== this.confirmPassword) {
+      if (this.email.length < 3 || !~this.email.indexOf("@")) {
         uni.showToast({
           icon: "none",
-          title: "两次密码输入不一致",
+          title: "邮箱地址不合法",
         });
         return;
       }
-
-      const data = {
-        username: this.username,
-        password: this.password,
-      };
-      // TODO: 注册
-      // uniCloud.callFunction({
-      // 	name: 'user-center',
-      // 	data: {
-      // 		action: 'register',
-      // 		params: data
-      // 	},
-      // 	success(e) {
-      // 		console.log("注册", e);
-
-      // 		if (e.result.code === 0) {
-      // 			uni.showToast({
-      // 				title: '注册成功'
-      // 			});
-      // 			uni.setStorageSync('uni_id_token', e.result.token)
-      // 			uni.setStorageSync('username', e.result.username)
-      // 			uni.reLaunch({
-      // 				url: '../main/main',
-      // 			});
-      // 		} else {
-      // 			uni.showModal({
-      // 				content: JSON.stringify(e.result),
-      // 				showCancel: false
-      // 			})
-      // 		}
-      // 	},
-      // 	fail(e) {
-      // 		uni.showModal({
-      // 			content: JSON.stringify(e),
-      // 			showCancel: false
-      // 		})
-      // 	}
-      // })
+    },
+    open() {
+      this.confirmBtnLoading = true;
+      // 检查输入是否合法
+      this.checkIsValid();
+      // 打开弹出框
+      this.$refs.popup.open();
+    },
+    confirm() {
+      this.close();
+      this.register();
+    },
+    close(done) {
+      // 关闭弹出框
+      this.$refs.popup.close();
+    },
+    register() {
+      let _self = this
+      uni.request({
+        method: "GET",
+        url: baseURL + "/register/",
+        data: {
+          username: this.username,
+          password: this.password,
+          email: this.email,
+        },
+        success(e) {
+          console.log("注册", e);
+          let res = e.data;
+          if (res.code === 0) {
+            uni.showToast({
+              title: "注册成功",
+            });
+          } else {
+            uni.showModal({
+              content: res.msg,
+              showCancel: false,
+            });
+          }
+        },
+        fail(e) {
+          uni.showModal({
+            content: e.data.msg,
+            showCancel: false,
+          });
+        },
+        complete(e) {
+          console.log("complete", _self.confirmBtnLoading);
+          _self.confirmBtnLoading = false;
+        },
+      });
     },
   },
 };
