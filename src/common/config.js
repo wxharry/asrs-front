6b9fcd749e4f04fa0cf8879891fe12b1ceb9f7c3
wxh <wxh170607@163.com>
2021-01-17 19:40:26 +0800

    delete unused parts

--- src/common/config.js
+++ src/common/config.js
@@ -2,8 +2,8 @@ let url_config = ""
 
 if(process.env.NODE_ENV === 'development'){
     // 开发环境
-    // url_config = '/api'
-    url_config = 'https://www.liaoluoxing.cn:444'
+    url_config = '/api'
+    // url_config = 'https://www.liaoluoxing.cn:444'
 }else{
     // 生产环境
     if (process.env.VUE_APP_PLATFORM === 'mp-weixin') {
