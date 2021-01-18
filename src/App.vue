65602bdd8db52deebc058583574fc2044225dbd8
TangYenan <1589972754@qq.com>
2021-01-18 20:03:39 +0800

    Delete some useless code. --tyn

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,8 @@ import { mapState, mapMutations } from "vuex";
 
 export default {
   onLaunch: function () {
+    console.log("App Launch");
+
     // 临时获取publicKey
     this.$request({
       url: "/test2/",
@@ -15,31 +17,8 @@ export default {
         console.log(e);
       },
     });
-
-    console.log("App Launch");
-    let uniIdToken = uni.getStorageSync("uni_id_token");
-    if (uniIdToken) {
-      this.login(uni.getStorageSync("username"));
-    }
-
-    // #ifdef APP-PLUS
-    // 一键登录预登陆，可以显著提高登录速度
-    uni.preLogin({
-      provider: "univerify",
-      success: (res) => {
-        this.setUniverifyErrorMsg();
-        this.setHideUniverify(false);
-        console.log("preLogin success: ", res);
-      },
-      fail: (err) => {
-        this.setUniverifyErrorMsg(err.errMsg);
-        // 没有开通一键登录
-        this.setHideUniverify(true);
-        console.log("preLogin fail: ", err);
-      },
-    });
-    // #endif
   },
+  
   onShow: function () {
     console.log("App Show");
   },
---
+++ src/pages/wxAuth/wxAuth.vue
@@ -0,0 +1,108 @@
+<template>
+	<view class="container">
+		<view class="left-top-sign">LOGIN</view>
+		<view class="welcome">欢迎回来！</view>
+		<button class="confirm-btn" open-type="getUserInfo" @getuserinfo="wxLogin" :disabled="logining">微信授权登录</button>
+	</view>
+</template>
+​
+<script>
+	// import { mapMutation } from 'vuex';​
+	export default {
+		data() {
+			return {
+				logining: false
+			};
+		},
+		onLoad() {},
+		methods: {
+			wxLogin(e) {
+				const that = this;
+				that.logining = true;
+				let userInfo = e.detail.userInfo;
+				uni.login({
+					provider: "weixin",
+					success: (login_res => {
+						let code = login_res.code;
+						console.log("code: ", code)
+						uni.getUserInfo({
+							success(info_res) {
+								console.log(info_res)
+								uni.request({
+									url: 'https://www.liaoluoxing.cn:444/wxlogin/',
+									method: "GET",
+									// header: {
+									// 	'content-type': 'application/x-www-form-urlencoded'
+									// },
+									data: {
+										code: code,
+										rawData: info_res.rawData
+									},
+									success(res) {
+										console.log(res.data)
+										// if (res.data.status == 200) {
+										// 	that.$store.commit('login', userInfo);
+										// 	// uni.setStorageSync("userInfo",userInfo);
+										// 	// uni.setStorageSync("skey", res.data.data);
+										// } else {
+										// 	console.log('服务器异常')
+										// }
+									},
+									fail(error) {
+										console.log(error)
+									}
+								})
+								uni.hideLoading()
+								uni.navigateBack()
+							}
+						})
+
+					})
+				})
+			}
+		}
+	};
+</script>
+​
+<style lang="scss">
+	.container {
+		display: flex;
+		overflow: hidden;
+		background: #fff;
+		flex-direction: column;
+		justify-content: center;
+
+		.left-top-sign {
+			font-size: 120upx;
+			color: blue($color: #000000);
+			position: relative;
+			left: -10upx;
+			margin-top: 100upx;
+		}
+
+		.welcome {
+			position: relative;
+			left: 50upx;
+			top: -90upx;
+			font-size: 46upx;
+			color: #555;
+			text-shadow: 1px 0px 1px rgba(0, 0, 0, 0.3);
+		}
+
+		.confirm-btn {
+			width: 630upx;
+			height: 76upx;
+			line-height: 76upx;
+			border-radius: 50px;
+			margin-top: 70upx;
+			background: green($color: #000000);
+			color: #fff;
+			font-size: 30upx;
+
+			&:after {
+				border-radius: 100px;
+			}
+		}
+	}
+</style>
+​
